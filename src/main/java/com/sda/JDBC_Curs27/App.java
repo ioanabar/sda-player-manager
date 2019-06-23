bb5f97128d53c16626a194eacca55e2ec65ccb82
unknown <Ioanabar>
2019-06-23 10:51:30 +0300

    Initial commit

---
+++ src/main/java/com/sda/JDBC_Curs27/App.java
@@ -0,0 +1,13 @@
+package com.sda.JDBC_Curs27;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
